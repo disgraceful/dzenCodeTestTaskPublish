796992e62feff49c55dbe40f3e0d05335089d9b0
disgraceful <noolic97@gmail.com>
2024-07-27 17:20:29 +0300

    Updates

--- assets/BIb8G_cQ.js
+++
@@ -1,1 +0,0 @@
-import{A as r,B as t}from"./BIe2YOIY.js";const d=r((e,a)=>{if(e.fullPath==="/")return t("/orders")});export{d as default};
---
+++ assets/CH0spAaM.js
@@ -0,0 +1,1 @@
+import{J as r,K as t}from"./BLKzFLAi.js";const d=r((e,a)=>{if(e.fullPath==="/")return t("/orders")});export{d as default};
